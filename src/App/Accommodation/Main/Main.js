71ff2e67d2e0d6cbc9d74597f89bac5e5f8ebd69
Christine <50479077+christinebogdan@users.noreply.github.com>
2021-06-01 10:47:01 +0200

    markup for main added, tag, rating & collapse components integrated

--- src/App/Accommodation/Main/Main.js
+++ src/App/Accommodation/Main/Main.js
@@ -1,14 +1,48 @@
 import React from "react";
 import "./Main.scss";
 import Tag from "./Tag/Tag";
+import json from "../../../data.json";
+import Rating from "./Rating/Rating";
+import Collapse from "./Collapse/Collapse";
 
 class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.id = this.props.id;
+    this.rating = this.props.rating;
+    this.range = this.props.range;
+    this.tags = this.props.tags;
+    this.description = this.props.description;
+    this.amenities = this.props.amenities;
+  }
   render() {
     return (
-      <div>
-        <div className="main">This is the Main part</div>
-        <Tag />
-      </div>
+      <main>
+        <div className="intro">
+          <div className="intro__header">
+            <h1 className="intro__header--title">{json[this.id].title}</h1>
+            <p className="intro__header--location">{json[this.id].location}</p>
+            <div className="intro__header--tags">
+              <Tag tags={this.tags} />
+            </div>
+          </div>
+          <div className="intro__profile">
+            <div className="intro__host">
+              <p className="intro__host--name">{json[this.id].host.name}</p>
+              <div className="intro__host--picture">
+                <img src={json[this.id].host.picture} alt=""></img>
+              </div>
+            </div>
+            <div className="intro__rating">
+              <Rating rating={this.rating} range={this.range} />
+            </div>
+          </div>
+        </div>
+        <div className="collapse">
+          <Collapse title="Description" content={this.description} />
+          <Collapse title="Amenities" content={this.amenities} />
+        </div>
+      </main>
     );
   }
 }
