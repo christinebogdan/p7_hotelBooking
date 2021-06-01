c9992b21a7e34dce961c2fb63bb52dba7575ad44
Christine <50479077+christinebogdan@users.noreply.github.com>
2021-06-01 10:49:20 +0200

    content wrapped in <main> element

--- src/App/Home/Main/Main.js
+++ src/App/Home/Main/Main.js
@@ -6,21 +6,23 @@ import React from "react";
 class Main extends React.Component {
   render() {
     return (
-      <ul className="home__main">
-        {json.map((accommodation, index) => (
-          // do I have to set the key here? Does not show up in the markup
-          <Card
-            title={accommodation.title}
-            cover={accommodation.cover}
-            // is it better to use the index of the array in the json or the
-            // element id property in each accommodation object?
-            id={index}
-            // id={accommodation.id}
-            key={index}
-            // key={accommodation.id}
-          />
-        ))}
-      </ul>
+      <main>
+        <ul className="home__main">
+          {json.map((accommodation, index) => (
+            // do I have to set the key here? Does not show up in the markup
+            <Card
+              title={accommodation.title}
+              cover={accommodation.cover}
+              // is it better to use the index of the array in the json or the
+              // element id property in each accommodation object?
+              id={index}
+              // id={accommodation.id}
+              key={index}
+              // key={accommodation.id}
+            />
+          ))}
+        </ul>
+      </main>
     );
   }
 }
