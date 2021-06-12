6c35581ef93c54634a4881fa2fbb5c3e773839cb
Christine <50479077+christinebogdan@users.noreply.github.com>
2021-06-12 11:30:01 +0200

    added constructor

--- src/App/Components/Card.js
+++ src/App/Components/Card.js
@@ -3,15 +3,21 @@ import "../Styles/Card.scss";
 import { Link } from "react-router-dom";
 
 class Card extends React.Component {
+  constructor(props) {
+    super(props);
+    this.id = this.props.id;
+    this.cover = this.props.cover;
+    this.title = this.props.title;
+  }
   render() {
     return (
       // where do I need to set the key? Here or in Main?
       <li className="card">
-        <Link to={`/accommodations/${this.props.id}`} className="card__link">
+        <Link to={`/accommodations/${this.id}`} className="card__link">
           <div className="card__wrap">
-            <img src={this.props.cover} alt="" className="card__img"></img>
+            <img src={this.cover} alt="" className="card__img"></img>
             <div className="card__layer"></div>
-            <h2 className="card__title">{this.props.title}</h2>
+            <h2 className="card__title">{this.title}</h2>
           </div>
         </Link>
       </li>
