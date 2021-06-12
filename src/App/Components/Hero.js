911527db5eea00caa8ad9c9a9c91eff97166f535
Christine <50479077+christinebogdan@users.noreply.github.com>
2021-06-12 11:34:54 +0200

    start implementation dynamic styling of component

--- src/App/Components/Hero.js
+++ src/App/Components/Hero.js
@@ -4,15 +4,41 @@ import "../Styles/Hero.scss";
 
 // do I need to set h1 with z-index or is there
 // another way to place above the layer?
+
+// how can I concatenate className with prop and string?
+
 class Hero extends React.Component {
+  constructor(props) {
+    super(props);
+    this.context = this.props.context;
+  }
   render() {
     return (
-      <div className="hero">
-        <div className="hero__img__layer"></div>
-        <h1 className="hero__headline">Home anywhere and everywhere</h1>
+      <div className={`${this.context}`}>
+        {/* <div className=`${this.context}__img__layer`></div> */}
+        <h1 className="homepage__headline">Home anywhere and everywhere</h1>
       </div>
     );
   }
 }
 
+// class Hero extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.context = this.props.context;
+//   }
+//   render() {
+//     return (
+//       <div className="hero">
+//         <div className="hero__img__layer"></div>
+//         <h1 className="hero__headline">Home anywhere and everywhere</h1>
+//       </div>
+//     );
+//   }
+// }
+
 export default Hero;
+
+// {
+//   `/accommodations/${this.id}`;
+// }
