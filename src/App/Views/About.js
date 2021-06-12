63ca935251a1f81ee99badabc7f73056c560f7f5
Christine <50479077+christinebogdan@users.noreply.github.com>
2021-06-12 11:31:32 +0200

    dynamic rendering of collapse input

--- src/App/Views/About.js
+++ src/App/Views/About.js
@@ -3,9 +3,9 @@ import Collapse from "../Components/Collapse";
 import "../Styles/About.scss";
 
 class About extends React.Component {
-  render() {
-    console.log("About is loaded");
-    const textInput = {
+  constructor(props) {
+    super(props);
+    this.textInput = {
       Reliability:
         "The ads posted on Kasa guarantee total reliability of the place. The photos are consistend with the accommodations, and all information is regularly checked by our teams.",
       Respect:
@@ -15,13 +15,14 @@ class About extends React.Component {
       Safety:
         "Safety is Kasa's priority. Both for our guests and for travelers, each accommodation is compliant with the safety criteria established by our services. By leaving a note for both the host and the tenant, our teams can verify that the standards are respected. We also organize workshops on home security for our guests.",
     };
-
+  }
+  render() {
     return (
       <ul className="about__main">
-        {Object.keys(textInput).map((key, index) => {
+        {Object.keys(this.textInput).map((key, index) => {
           return (
             <li key={key}>
-              <Collapse title={key} content={textInput[key]} />
+              <Collapse title={key} content={this.textInput[key]} />
             </li>
           );
         })}
