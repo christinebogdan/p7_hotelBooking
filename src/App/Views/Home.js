d5e384f5b467ee7530e94c2285d1053195bf481b
Christine <50479077+christinebogdan@users.noreply.github.com>
2021-06-12 11:33:16 +0200

    added prop in Hero component to determine styling

--- src/App/Views/Home.js
+++ src/App/Views/Home.js
@@ -7,7 +7,7 @@ class Home extends React.Component {
   render() {
     return (
       <div className="home">
-        <Hero />
+        <Hero context="homepage" />
         <Main />
       </div>
     );
